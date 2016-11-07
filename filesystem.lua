d5b640857b0abb72dd61e8795baa08ffd160f236
zorggn <zorg@atw.hu>
2016-11-07 19:17:36 +0100

    Added Fsys.unmount
    TODO:
    - test whether it works like this or not;
    - add function to change the write directory.

--- filesystem.lua
+++ filesystem.lua
@@ -13,22 +13,64 @@ local ffi = require "ffi"
 local liblove = ffi.os == "Windows" and ffi.load("love") or ffi.C
 
 ffi.cdef[[
+
+	typedef struct {
+		unsigned char major,
+		unsigned char minor,
+		unsigned char patch
+	} PHYSFS_Version;
+
+	void PHYSFS_getLinkedVersion(PHYSFS_Version * ver);
+
+	const char * PHYSFS_getLastError(void);
+
 	int PHYSFS_mount(
 		const char * newDir, const char * mountPoint, int appendToPath
-	)
-	const char * PHYSFS_getLastError(void)
+	);
+
+	int PHYSFS_removeFromSearchPath(const char * oldDir);
+	int PHYSFS_unmount(const char * oldDir);
 ]]
+
+-- TODO: If cdef dies like this due to either rFSP or unmnt missing, then
+--       we need an init funtion that cdef-s depending on the version;
+--       also modify Fsys.unmount to move the versioncheck out into init.
 
 local Fsys = {}
 
 Fsys.mount = function(path, mountPoint, appendToPath)
 	local result = liblove.PHYSFS_mount(path, mountPoint, appendToPath)
+
 	if result ~= 0 then 
 		return true
 	else
-		return liblove.PHYSFS_getLastError()
+		return false, liblove.PHYSFS_getLastError()
 	end
 end
+
+Fsys.unmount = function(path)
+	local result
+	local version = ffi.new('struct PHYSFS_Version[1]') -- may be *[1] instead.
+	liblove.PHYSFS_Version(version)
+
+	-- Depends on what Löve was built with.
+	if version[0].major >= 2 and version[0].minor >= 1 then
+		result = liblove.PHYSFS_unmount(path)
+	else
+		result = liblove.PHYSFS_removeFromSearchPath(path)
+	end
+
+	if result ~= 0 then 
+		return true
+	else
+		return false, liblove.PHYSFS_getLastError()
+	end
+end
+
+-- - Be aware of file/dir dropping causing non-optimal behaviour:
+--   In those two callbacks, the new mouse position may have not been polled
+--   yet by SDL, so we need to set a flag, and handle everything after one
+--   loop has happened.
 
 -- Load -> file dialog (project files allowed only)
 -- Save -> overwrite current file
